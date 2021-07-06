a27335d2839ed94aa0b3832991898542000f9674
vaibhavnathj-amsbio <vaibhavnathj@amsbio.com>
2021-07-06 10:52:57 +0100

    updated value2 field

--- my_module/models/models.py
+++ my_module/models/models.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 class my_module(models.Model):
     _name = 'my_module.my_module'
 
     name = fields.Char()
     value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
+    value2 = fields.Float()
     description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        self.value2 = float(self.value) / 100
